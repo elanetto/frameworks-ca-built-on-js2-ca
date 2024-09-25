c42ca8567f3c1ca202ef4aa172fd66cf9fc8ec93
Jonas Nettli <jonas.nettli@gmail.com>
2024-09-25 13:53:37 +0200

    feat: Added login functionality

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -3,15 +3,15 @@ export async function login({ email, password }) {
         const response = await fetch(API_AUTH_LOGIN, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${API_KEY}`,
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({ email, password }),
         });
         const data = await response.json();
         if (data.success) {
+            const authToken = data.data.accessToken;
+            localStorage.setItem('authToken', authToken);
             console.log('User logged in successfully!');
-            return data.data;
         } else {
             throw new Error('Invalid username or password');
         }
--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,1 +1,31 @@
-export async function onLogin(event) {}
+import { API_AUTH_LOGIN } from "../../api/constants";
+
+export async function onLogin(event) {
+    event.preventDefault();
+
+    const email = document.forms.login.email.value;
+    const password = document.forms.login.password.value;
+
+    fetch(API_AUTH_LOGIN, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Login response:', data); // Add this line to log the response
+        if (data.data) {
+            alert("User logged in successfully!");
+            localStorage.setItem('authToken', data.data.accessToken);
+            // window.location.href = '/dashboard';
+        } else {
+            console.log(data)
+            throw new Error('Invalid username or password');
+        }
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
