5ce0878e94e8bc3e51dd2be001389c8cbaab2693
elanetto <101054274+elanetto@users.noreply.github.com>
2024-09-30 16:20:38 +0200

    fix: Fixed login while trying to fix create post. Needed to be able to login and store username and token to local storage before I could create the create post code.

--- src/js/api/auth/key.js
+++ src/js/api/auth/key.js
@@ -1,1 +1,8 @@
-export async function getKey(name) {}
+export async function getKey() {
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) {
+        console.error("Access token not found.");
+        return null;  
+    }
+    return accessToken;
+}
--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,1 +1,24 @@
-export async function onLogin(event) {}
+import { login } from '../../api/auth/login.js';
+
+// test
+console.log("JS-login page under ui/auth/login.js loaded");
+
+export async function onLogin(event) {
+  event.preventDefault(); 
+  
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    const result = await login({ email, password });
+
+    if (result.error) {
+      alert("Login failed: " + result.error);
+    } else {
+      window.location.href = "/";
+    }
+  } catch (error) {
+    console.error('Login error:', error);
+    alert('An unexpected error occurred. Please try again later.');
+  }
+}
