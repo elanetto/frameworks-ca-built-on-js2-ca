13d524cab14472ebafc7a7a11b25754be8359364
elanetto <101054274+elanetto@users.noreply.github.com>
2024-09-30 16:22:44 +0200

    Fix: fixed login code

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -1,4 +1,5 @@
 export function authGuard() {
+  // fixed name from token to accessToken
   if (!localStorage.accessToken) {
     alert("You must be logged in to view this page");
     window.location.href = "/auth/login/";
